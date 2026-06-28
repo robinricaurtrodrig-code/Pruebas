3f78f73a3a8b8520c9190fd6397acd30a75eadb6
robinricaurtrodrig-code <robinson.ricaurte.rodriguez@gmail.com>
2026-06-28 12:11:29 -0500

    Delete test_conn.php

--- test_conn.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . '/config/database.php';
-try {
-    $db = Database::getConnection();
-    echo "Conexion OK a MySQL\n";
-} catch (Exception $e) {
-    echo "ERROR: " . $e->getMessage() . "\n";
-}
